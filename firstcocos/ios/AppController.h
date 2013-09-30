6a21cebf548ff4b88836eb1f5319207aa03fb87e
nie xin <niexin.study@gmail.com>
2013-09-30 23:27:13 +0200

    first try on cocos

---
+++ firstcocos/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  firstcocosAppController.h
+//  firstcocos
+//
+//  Created by Genie on 30/09/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ firstcocos/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  firstcocosAppController.h
+//  firstcocos
+//
+//  Created by Genie on 30/09/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
